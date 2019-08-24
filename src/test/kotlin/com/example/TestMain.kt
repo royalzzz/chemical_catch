22c056ef4dbaf5f7d02395df1d1e56bd49983373
Royal-XPS <824259085@qq.com>
2019-08-24 15:55:19 +0800

    [添加代理] 还剩一千个化学品

--- src/test/kotlin/com/example/TestMain.kt
+++ src/test/kotlin/com/example/TestMain.kt
@@ -29,19 +29,20 @@ class TestMain {
         var i = 0
         for (cas in casList) {
             if (cas.isNotEmpty()) {
-                print(++i)
+                print("${++i} / ${casList.size}")
                 print("\t")
                 println(cas)
                 getInfo(url, "ghs", cas)
-                val time = ((Math.random() * 30 + 10) * 1000).toLong()
-                println("sleep: $time ms")
-                sleep(time);
+//                val time = ((Math.random() * 30 + 10) * 1000).toLong()
+//                println("sleep: $time ms")
+                sleep(5000);
             }
         }
 
 //        val url = "http://www.hgmsds.com/getEhsDetails"
 //        getInfo(url, "ghs", "513-88-2")
         // 17853678882
+        // 15615125129
         //JSESSIONID=71EC9A261F66F01859BDDBDA29C5F8E6; Qs_lvt_56176=1566448872; Qs_pv_56176=1962859087683456000%2C3067919151266341000%2C4218948666408735000%2C4398872970285031400%2C4381712557310873600; mediav=%7B%22eid%22%3A%22105583%22%2C%22ep%22%3A%22%22%2C%22vid%22%3A%22LMRMfqzlwr%3Ahi%5BQkC%23uv%22%2C%22ctn%22%3A%22%22%7D; Hm_lvt_f27a00454fe3332070be8b71a0c64602=1566448873; Hm_lpvt_f27a00454fe3332070be8b71a0c64602=1566456088; Hm_lvt_581f7711e663e3f8f681a6c26d63b804=1566448873; Hm_lpvt_581f7711e663e3f8f681a6c26d63b804=1566456089
         //JSESSIONID=71EC9A261F66F01859BDDBDA29C5F8E6; Qs_lvt_56176=1566448872; Qs_pv_56176=3067919151266341000%2C4218948666408735000%2C4398872970285031400%2C4381712557310873600%2C2202059588098944300; mediav=%7B%22eid%22%3A%22105583%22%2C%22ep%22%3A%22%22%2C%22vid%22%3A%22LMRMfqzlwr%3Ahi%5BQkC%23uv%22%2C%22ctn%22%3A%22%22%7D; Hm_lvt_f27a00454fe3332070be8b71a0c64602=1566448873; Hm_lpvt_f27a00454fe3332070be8b71a0c64602=1566456192; Hm_lvt_581f7711e663e3f8f681a6c26d63b804=1566448873; Hm_lpvt_581f7711e663e3f8f681a6c26d63b804=1566456192
     }
